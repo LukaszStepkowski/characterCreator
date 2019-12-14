a7bbbd05b34d8acba1eed90166a8315f8412899e
Lukasz Stepkowski <l.stepkow@gmail.com>
2019-12-14 14:05:17 +0100

    added fields to Character class, created RaceTypes enum, created Statistics class

--- src/character/Character.java
+++ src/character/Character.java
@@ -1,4 +1,15 @@
 package character;
 
+import enums.RaceTypes;
+
+import java.util.List;
+
 public class Character {
+
+    private String name;
+    private int age;
+    private RaceTypes race;
+    private List<Statistics> startingStatistics;
+
+
 }
