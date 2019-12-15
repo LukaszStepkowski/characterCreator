3c199f9a84d582deec9e02c7823f9923b737916d
Lukasz Stepkowski <l.stepkow@gmail.com>
2019-12-15 14:24:26 +0100

    added proffesion, Appereance, weapons, armour, expirence, money, skills and Traits fields to Character class, created corresponding classes

--- src/character/Character.java
+++ src/character/Character.java
@@ -8,10 +8,17 @@ import java.util.List;
 public class Character {
 
     private String name;
-    private int age;
     private RaceTypes race;
+    private Proffesion proffesion;
     private List<Statistics> startingStatistics;
     private List<Item> inventory;
-
+    private Appearance appearance;
+    private List<Item> weaponsOnHand;
+    private List<Item> armourOnCharacter;
+    private int currentExp;
+    private int totalExp;
+    private int money;
+    private List<Skills> skills;
+    private List<Traits> traits;
 
 }
